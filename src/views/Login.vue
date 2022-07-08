9f8a956a08e8da5887f3002e60589ffbe088e4c2
wangshumin <1793033075@qq.com>
2022-07-08 15:21:14 +0800

    test

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -46,7 +46,7 @@
                 loading:false,
                 checked: true,
                 rules: {
-                    username: [{required: true, message: '请输入用户名', trigger: 'blur'},],
+                    username: [{required: true, message: '请输入用户名!', trigger: 'blur'},],
                     password: [{required: true, message: '请输入密码', trigger: 'blur'}],
                     code: [{required: true, message: '请输入验证码', trigger: 'blur'}]
                 }
